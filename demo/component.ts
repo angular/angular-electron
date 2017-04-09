7ca558140b2522d298a608d952c95dc9f53684e6
nayunhwan <nayunhwan.dev@gmail.com>
2017-04-09 14:50:16 +0900

    Update'

--- demo/component.ts
+++ demo/component.ts
@@ -2,13 +2,16 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'app',
-	template: `<div>Hello from {{name}}</div>`
+	template: `<div>Hello from {{name}} {{id}}</div>`
 })
+
 export class App {
 	name: string;
+	id: string;
+
 	constructor(){
 		this.name = 'Angular2 Electron!';
-		
+    this.id = "Good to see you";
 		setTimeout(() => {
 			this.name = 'Angular2 Electron!!!';
 		},1000);
